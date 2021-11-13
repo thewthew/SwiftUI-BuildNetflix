2a5da661f80998e0f12774ca0229f728ba691eef
Matthew Usdin <>
2021-11-13 10:58:59 +0100

    [Section 2 - Building the 'Home' View] : Large Movie Preview (Part 1)

--- BuildNetflix/Home/GlobalHelpers.swift
+++ BuildNetflix/Home/GlobalHelpers.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300")!)
-let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/301")!)
-let exampleMovie3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/302")!)
-let exampleMovie4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/303")!)
-let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/304")!)
-let exampleMovie6 = Movie(id: UUID().uuidString, name: "Atfer Life", thumbnailURL: URL(string: "https://picsum.photos/200/305")!)
+let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300")!, categories: ["Dystopian", "Exciting", "Suspenful", "Sci-fi TV"])
+let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/301")!, categories: ["Dystopian", "Exciting", "Suspenful", "Sci-fi TV"])
+let exampleMovie3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/302")!, categories: ["Dystopian", "Exciting", "Suspenful", "Sci-fi TV"])
+let exampleMovie4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/303")!, categories: ["Dystopian", "Exciting", "Suspenful", "Sci-fi TV"])
+let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/304")!, categories: ["Dystopian", "Exciting", "Suspenful", "Sci-fi TV"])
+let exampleMovie6 = Movie(id: UUID().uuidString, name: "Atfer Life", thumbnailURL: URL(string: "https://picsum.photos/200/305")!, categories: ["Dystopian", "Exciting", "Suspenful", "Sci-fi TV"])
 
 let exampleMovies: [Movie] = [exampleMovie1,
                               exampleMovie2,
